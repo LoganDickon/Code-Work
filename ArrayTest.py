3717310f074b7e48c2824347878767e6997b7ec0
LoganDickon <83386021+LoganDickon@users.noreply.github.com>
2021-05-15 12:37:54 -0500

    Update ArrayTest.py

--- ArrayTest.py
+++ ArrayTest.py
@@ -1,75 +1,74 @@
-"""
-Created on Wed May 12 08:33:18 2021
+#===============================================================================================================================#
 
-@author: Logan D
-"""
+print('-------------------------------------------------')
+print("Modified by Logan D on: 5/14/2021\n")
 
-import numpy as np 
-import cv2
-from threading import Thread
-import cProfile, pstats, io
+#===============================================================================================================================#
 
-def Profile(fnc):
-    ###A decorator that uses cProfile to profile a function###
-    def inner(*args, **kwargs):
-        pr = cProfile.Profile()
-        pr.enable()
-        retval = fnc(*args, **kwargs)
-        pr.disable()
-        s = io.StringIO()
-        sortby = 'cumulative'
-        ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
-        ps.print_stats()
-        print(s.getvalue())
-        return retval
-    return inner
+from timeit import default_timer #Library for runtime Timer
+start = default_timer() #Start runtime Timer
+from numpy import zeros, uint8 #Library for creating and manipulating arrays
+from cv2 import VideoCapture, CAP_DSHOW #Library for webcam functionality
+from threading import Thread #Library that allows multithreading
 
-def HudCam():
-    cam_Num = 0
-    hud_Cam = cv2.VideoCapture(cam_Num, cv2.CAP_DSHOW) 
+#===============================================================================================================================#
+
+W, H = 640, 480 #Resolution of both camera's
+cam_Num = 0 #Gauge camera's camera number
+cam_Num_2 = 1 #Gauge camera's camera number
+
+#===============================================================================================================================#
+
+def HudCam(W, H, cam_Num): #Function to create an array from hud cam
+    hud_Cam = VideoCapture(cam_Num, CAP_DSHOW) #Call hud cam
     #If Hud camera is not found
-    if not hud_Cam.isOpened():
-        #Log error to console
-        raise RuntimeError('Error opening Hud Camera. Check USB port!')
+    if not hud_Cam.isOpened(): #If hud camera resource is not found
+        raise RuntimeError('Error opening Hud Camera. Check USB port!') #Log error to console
     #Set resolution of camera to 720p
-    W, H = 1280, 720
-    hud_Cam.set(cv2.CAP_PROP_FRAME_WIDTH, W)
-    hud_Cam.set(cv2.CAP_PROP_FRAME_HEIGHT, H)
+    #hud_Cam.set(3, W) #Set aspect ratio and resolution of x-Coordinate
+    #hud_Cam.set(4, H) #Set aspect ratio and resolution of y-Coordinate
     #Read/Grab Hud camera frame
-    rval, hud_Img = hud_Cam.read()
-    hud_Cam.release()
-    hud_Array = np.zeros(hud_Img.shape, dtype=np.uint8)
-    hud_Array = hud_Img.copy()
-    print(type(hud_Array))
-    print(hud_Array.shape)
-    print(hud_Array)
+    rval, hud_Img = hud_Cam.read() #Read gauge camera frame
+    hud_Cam.release() #Release gauge camera resource
+    hud_Array = zeros(hud_Img.shape, dtype=uint8) #Create an array template
+    hud_Array = hud_Img.copy() #Fill array template with gauge camera rgb values
+    print("Hud Camera\'s resolution is:", hud_Array.shape) #Log resolution to console 
+    #print(hud_Array) #Log hud camera's array to console
+    
+#===============================================================================================================================#
 
-def GaugeCam():
-    cam_Num = 1
-    hud_Cam = cv2.VideoCapture(cam_Num, cv2.CAP_DSHOW) 
-    #If Hud camera is not found
-    if not hud_Cam.isOpened():
-        #Log error to console
-        raise RuntimeError('Error opening Hud Camera. Check USB port!')
+def GaugeCam(W, H, cam_Num_2): #Function to create an array from gauge cam
+    gauge_Cam = VideoCapture(cam_Num_2, CAP_DSHOW) #Open gauge camera
+    if not gauge_Cam.isOpened(): #If gauge camera is not found
+        raise RuntimeError('Error opening Gauge Camera. Check USB port!') #Log error to console
     #Set resolution of camera to 720p
-    W, H = 1280, 720
-    hud_Cam.set(cv2.CAP_PROP_FRAME_WIDTH, W)
-    hud_Cam.set(cv2.CAP_PROP_FRAME_HEIGHT, H)
-    #Read/Grab Hud camera frame
-    rval, hud_Img = hud_Cam.read()
-    hud_Cam.release()
-    hud_Array = np.zeros(hud_Img.shape, dtype=np.uint8)
-    hud_Array = hud_Img.copy()
-    print(type(hud_Array))
-    print(hud_Array.shape)
-    print(hud_Array)
+    #gauge_Cam.set(3, W) #Set aspect ratio and resolution of x-Coordinate
+    #gauge_Cam.set(4, H) #Set aspect ratio and resolution of y-Coordinate
+    rval, gauge_Img = gauge_Cam.read() #Read gauge camera frame
+    gauge_Cam.release() #Release gauge camera resource
+    gauge_Array = zeros(gauge_Img.shape, dtype=uint8) #Create an array template
+    gauge_Array = gauge_Img.copy() #Fill array template with gauge camera rgb values
+    print('-------------------------------------------------')
+    print("Gauge Camera\'s resolution is:", gauge_Array.shape) #Log resolution to console
+    print('-------------------------------------------------')
+    print()
+    
+#===============================================================================================================================#
 
+def main(start): #Function that runs both camera's
+    hud_Thread = Thread(target = HudCam, args = (W, H, cam_Num)) #Assigns HudCam to run on it's own thread
+    gauge_Thread = Thread(target = GaugeCam, args = (W, H, cam_Num_2)) #Assigns GaugeCam to run on it's own thread
+    hud_Thread.start() #Starts running HudCam function in it's specified cpu thread
+    gauge_Thread.start() #Starts running GaugeCam function in it's specified cpu thread
+    hud_Thread.join() #When HudCam is finished release it's specified thread
+    gauge_Thread.join() #When GaugeCam is finished release it's specified thread
+    
+#===============================================================================================================================#
 
-def main():
-    t1 = Thread(target = HudCam)
-    t2 = Thread(target = GaugeCam)
-    t1.start()
-    t2.start()
-    t1.join()
-    t2.join()
-main()
+main(start)
+stop = default_timer() #Stop runtime Timer
+print('-------------------------------------------------')
+print('Time:', "{:.2f}".format(stop - start))
+print('-------------------------------------------------')
+    
+#===============================================================================================================================#
