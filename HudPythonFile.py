655301374dc6cad15d0d7c41b801abf1b3e8ad7b
LoganDickon <83386021+LoganDickon@users.noreply.github.com>
2021-05-23 18:21:00 -0500

    Update HudPythonFile.py

--- HudPythonFile.py
+++ HudPythonFile.py
@@ -1,5 +1,4 @@
 from cv2 import VideoCapture, CAP_DSHOW
-from numpy import zeros, uint8
 
 class VideoCaptureWrapper(object):
     def __init__(self, *args, **kwargs):
@@ -7,16 +6,15 @@ class VideoCaptureWrapper(object):
     def __enter__(self):
         return self
     def __exit__(self, *args):
-        self.Cam.release()
+        self.Cam.release() #Release camera resource
 
 def Hud_Picture():
-    with VideoCaptureWrapper(0, CAP_DSHOW) as Hud_camera:
-        _, H_Img = Hud_camera.Cam.read()
-    H_Array = zeros(H_Img.shape, dtype=uint8) #Create an array template
+    with VideoCaptureWrapper(0, CAP_DSHOW) as Hud_camera: #Context Manager to control the VideoCapture method
+        _, H_Img = Hud_camera.Cam.read() #Read Camera Frame
     H_Array = H_Img.copy() #Fill array template with gauge camera rgb values
-    print(H_Array)
-    return H_Array
+    print(H_Array) #Log to console
+    return H_Array #Return Array so LabVIEW can grab it
 
-if __name__ == "__main__":
-    Hud_Picture()
+if __name__ == "__main__": #Run if this is main .py file
+    Hud_Picture() #Call camera function
 
