f731a2dae14f9f0df19eda67d13aa729e611448c
LoganDickon <83386021+LoganDickon@users.noreply.github.com>
2021-06-07 23:04:36 -0500

    Update HudPythonFile.py

--- HudPythonFile.py
+++ HudPythonFile.py
@@ -11,9 +11,8 @@ class VideoCaptureWrapper(object):
 def Hud_Picture():
     with VideoCaptureWrapper(0, CAP_DSHOW) as Hud_camera: #Context Manager to control the VideoCapture method
         _, H_Img = Hud_camera.Cam.read() #Read Camera Frame
-    H_Array = H_Img.copy() #Fill array template with gauge camera rgb values
-    print(H_Array) #Log to console
-    return H_Array #Return Array so LabVIEW can grab it
+    print(H_Img) #Log to console
+    return H_Img #Return Array so LabVIEW can grab it
 
 if __name__ == "__main__": #Run if this is main .py file
     Hud_Picture() #Call camera function
