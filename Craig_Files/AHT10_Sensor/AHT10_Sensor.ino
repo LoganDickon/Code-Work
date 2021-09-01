65f46d6544aa67df732f7568ac7ac1b1d4640203
LoganDickon <83386021+LoganDickon@users.noreply.github.com>
2021-09-01 16:27:07 -0500

    Update AHT10_Sensor.ino

--- Craig_Files/AHT10_Sensor/AHT10_Sensor.ino
+++ Craig_Files/AHT10_Sensor/AHT10_Sensor.ino
@@ -1,25 +1,23 @@
-#include "sensor_asair_aht10.h"
-#define AHT10_I2C_BUS "i2c4"
+#include <Adafruit_AHT10.h>
 
-int rt_hw_aht10_port(void)
-{
-    struct rt_sensor_config cfg;
-
-    cfg.intf.dev_name = AHT10_I2C_BUS;
-    cfg.intf.user_data = (void *)AHT10_I2C_ADDR;
-    
-    rt_hw_aht10_init("aht10", &cfg);
-
-    return RT_EOK;
-}
-INIT_ENV_EXPORT(rt_hw_aht10_port);
+Adafruit_AHT10 aht;
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("Adafruit AHT10 demo!");
 
+  if (! aht.begin()) {
+    Serial.println("Could not find AHT10? Check wiring");
+    while (1) delay(10);
+  }
+  Serial.println("AHT10 found");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  sensors_event_t humidity, temp;
+  aht.getEvent(&humidity, &temp);// populate temp and humidity objects with fresh data
+  Serial.print("Temperature: "); Serial.print(temp.temperature); Serial.println(" degrees C");
+  Serial.print("Humidity: "); Serial.print(humidity.relative_humidity); Serial.println("% rH");
 
+  delay(500);
 }
